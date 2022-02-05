b89b2e068f21ea3b81594c530f4eec77beaa3b22
Domurba <91464837+Domurba@users.noreply.github.com>
2022-02-05 17:19:41 +0200

    Update dotv.py

--- main/AIRFLOW/plugins/DATABASE/dotv.py
+++ main/AIRFLOW/plugins/DATABASE/dotv.py
@@ -8,10 +8,9 @@ load_dotenv(Path(__file__).parents[1] / '.env')
 def get_uri():
     """Returns DB conn info URI from .env file, which is used in by docker"""
     load_dotenv()
-    return "postgresql://{}:{}@{}:{}/{}".format(
+    return "postgresql://{}:{}@{}:{}/postgres".format(
         os.getenv("POSTGRES_USER", "postgres"),
         os.getenv("POSTGRES_PASSWORD", "root"),
         os.getenv("POSTGRES_CONTAINER_NAME", "postgres"),
-        "5432",
-        os.getenv("POSTGRES_USER", "postgres"),
+        "5432"
     )
